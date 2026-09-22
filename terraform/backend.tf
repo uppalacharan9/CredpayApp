fe119f29a6657163d3d348486e8657b3e7ca5706
uppala <uppalasaicharanteja@gmail.com>
2026-09-21 21:12:12 -0500

    Latest Update 2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "credpay_rg"
+    resource_group_name  = "credpay"
     storage_account_name = "credpaycr1"
     container_name       = "statefile"
     key                  = "credpay.terraform.tfstate"
